b9197742534596832b6c1b7542bee8066001279f
pradeep <pradeepjangid@aladinnserver.in>
2024-10-26 15:25:26 +0530

    test

--- src/constants/Appassets.tsx
+++ src/constants/Appassets.tsx
@@ -1,18 +1,17 @@
 export class Appassets {
-    static logo = require(`../../public/home/Group (1).png`);
-    static footerimg = require(`../../public/home/Parenting made easy-yt 1.png`);
-    static footerappplayimg = require(`../../public/home/appplay.png`);
-    static footerplaylogoimg = require(`../../public/home/googleplay.png`);
-    static footermailimg = require(`../../public/home/mail-fill.png`);
-    static footerbottomimg = require(`../../public/home/footerlayer.png`);
-    static frontbannerimg = require(`../../public/home/1.png`);
-    static fronttabbanner1img = require(`../../public/home/tabbanner1img.png`);
-    static fronttabbanner2img = require(`../../public/home/Phone Mockup 2.png`);
-    static fronttabbanner3img = require(`../../public/home/tabbanner1img.png`);
-    static fronttabbanner4img = require(`../../public/home/Phone Mockup 2.png`);
-    static whychildimg1 = require(`../../public/home/Chillo Emojis.png`);
-    static whychildimg2 = require(`../../public/home/Chillo Emojis 2.png`);
-    static whychildimg3 = require(`../../public/home/Chillo Emojis 3.png`);
-    static whychildimg4 = require(`../../public/home/Chillo Emojis 4.png`);
-
+    static logo = require('../../public/home/Group (1).png').default;
+    static footerimg = require('../../public/home/Parenting made easy-yt 1.png').default;
+    static footerappplayimg = require('../../public/home/appplay.png').default;
+    static footerplaylogoimg = require('../../public/home/googleplay.png').default;
+    static footermailimg = require('../../public/home/mail-fill.png').default;
+    static footerbottomimg = require('../../public/home/footerlayer.png').default;
+    static frontbannerimg = require('../../public/home/1.png').default;
+    static fronttabbanner1img = require('../../public/home/tabbanner1img.png').default;
+    static fronttabbanner2img = require('../../public/home/Phone Mockup 2.png').default;
+    static fronttabbanner3img = require('../../public/home/tabbanner1img.png').default;
+    static fronttabbanner4img = require('../../public/home/Phone Mockup 2.png').default;
+    static whychildimg1 = require('../../public/home/Chillo Emojis.png').default;
+    static whychildimg2 = require('../../public/home/Chillo Emojis 2.png').default;
+    static whychildimg3 = require('../../public/home/Chillo Emojis 3.png').default;
+    static whychildimg4 = require('../../public/home/Chillo Emojis 4.png').default;
 }
