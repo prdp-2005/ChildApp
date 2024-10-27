fcac53e72de866c23c77ad93022d70cf4d6f9827
pradeep <pradeepjangid@aladinnserver.in>
2024-10-27 21:45:09 +0530

    planes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Stories from "@/components/HomeComponents/Stories";
 import Banner from "../components/HomeComponents/Banner";
 import TabBanners from "../components/HomeComponents/TabBanners";
 import Whychild from "../components/HomeComponents/Whychild";
+import Planes from "@/components/HomeComponents/Planes";
 export default function Home() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function Home() {
       <TabBanners />
       <Whychild />
       <Stories />
+      <Planes />
     </>
   );
 }
