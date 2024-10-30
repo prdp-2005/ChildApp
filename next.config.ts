477877d5786f63f865f6d69a9dbef11b4e69db58
pradeep <pradeepjangid@aladinnserver.in>
2024-10-30 12:20:49 +0530

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// Import Next.js types (if needed) for TypeScript compatibility
 import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
 
+// Export the configuration with any additional Next.js settings
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add other Next.js configurations here if needed
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Chilld - Be a child Perent",
   description: "Parenting made easy",
+  manifest: '/manifest.webmanifest'
 };
 
 export default function RootLayout({
